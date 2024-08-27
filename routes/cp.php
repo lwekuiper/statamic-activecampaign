dc0e0f62999f7963c9d6e7f7a075fbf0eaad9e55
Laurens Kuiper <l@urens.me>
2024-08-27 15:04:32 +0200

    Separate routes with empty line

--- routes/cp.php
+++ routes/cp.php
@@ -8,6 +8,7 @@ use Lwekuiper\StatamicActivecampaign\Http\Controllers\GetMergeFieldsController;
 Route::name('activecampaign.')->prefix('activecampaign')->group(function () {
     Route::get('edit', [ActiveCampaignController::class, 'edit'])->name('edit');
     Route::patch('update', [ActiveCampaignController::class, 'update'])->name('update');
+
     Route::get('form-fields/{form}', [GetFormFieldsController::class, '__invoke'])->name('form-fields');
     Route::get('merge-fields', [GetMergeFieldsController::class, '__invoke'])->name('merge-fields');
 });
