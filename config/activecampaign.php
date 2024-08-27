cbcea7ae07dd8148a117aefea3a7ab57a7cc5734
Laurens Kuiper <l@urens.me>
2024-08-27 15:03:29 +0200

    Add comment blocks to config file

--- config/activecampaign.php
+++ config/activecampaign.php
@@ -3,50 +3,71 @@
 return [
 
     /**
-     *
+     * ActiveCampaign API URL
      */
     'api_url' => env('ACTIVECAMPAIGN_API_URL'),
 
     /**
-     *
+     * ActiveCampaign API Key
      */
     'api_key' => env('ACTIVECAMPAIGN_API_KEY'),
 
     /**
-     *
+     * The form submissions to add to your ActiveCampaign lists
      */
     'forms' => [
+        [
+            /**
+             * The form handle.
+             */
+            'form' => '',
 
-        /**
-         *
-         */
-        'form' => '',
+            /**
+             * Field name that contains the email.
+             */
+            'email_field' => '',
 
-        /**
-         *
-         */
-        'email_field' => '',
+            /**
+             * Field name that contains the consent.
+             */
+            'consent_field' => '',
 
-        /**
-         *
-         */
-        'consent_field' => '',
+            /**
+             * A ActiveCampaign list.
+             */
+            'list_id' => '',
 
-        /**
-         *
-         */
-        'list_id' => '',
+            /**
+             * A ActiveCampaign tag.
+             */
+            'tag' => '',
 
-        /**
-         *
-         */
-        'tag' => '',
+            /**
+             * Merge fields to add to the contact.
+             */
+            'merge_fields' => [],
+        ]
+    ],
 
-        /**
-         *
-         */
-        'merge_fields' => [],
+    /**
+     * The multi-site forms to add to your ActiveCampaign lists - requires pro edition
+     */
+    'sites' => [
+        'default' => [
+            [
+                'form' => '',
 
+                'email_field' => '',
+
+                'consent_field' => '',
+
+                'list_id' => '',
+
+                'tag' => '',
+
+                'merge_fields' => [],
+            ]
+        ]
     ]
 
 ];
